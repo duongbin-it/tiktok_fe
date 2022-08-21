16eb150322edbd2ca59afb618f6f44b9fa04fcbc
duongbin_it <hackingblackjackhvm@gmail.com>
2022-08-22 03:56:01 +0700

    update code + maintain + config api + uuidv4

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,10 @@
-const SUGGEST_ACOUNTS = 'https://tiktok-nodejs1.herokuapp.com/api/suggest_acounts'
-const DISCOVER = 'https://tiktok-nodejs1.herokuapp.com/api/discover'
-const NEWFEED = 'https://tiktok-nodejs1.herokuapp.com/api/newfeed'
-const USERS = 'https://tiktok-nodejs1.herokuapp.com/api/users'
-const FOLLOWING = 'https://tiktok-nodejs1.herokuapp.com/api/following'
-const HEARTED = 'https://tiktok-nodejs1.herokuapp.com/api/hearted'
+const URL = 'https://tiktok-nodejs1.herokuapp.com/'
+
+const SUGGEST_ACOUNTS = `${URL}/api/suggest_acounts`
+const DISCOVER = `${URL}/api/discover`
+const NEWFEED = `${URL}/api/newfeed`
+const USERS = `${URL}/api/users`
+const FOLLOWING = `${URL}/api/following`
+const HEARTED = `${URL}/api/hearted`
 
 export { SUGGEST_ACOUNTS, DISCOVER, NEWFEED, USERS, FOLLOWING, HEARTED }
