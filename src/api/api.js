5cc911b888464475ebd8cc4ea766282db7f48664
duongbin_it <hackingblackjackhvm@gmail.com>
2022-09-09 19:02:55 +0700

    commit : update

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,14 @@
-const URL = 'https://tiktok-be.fly.dev'
+const URL = 'https://tiktokbe.fly.dev'
 // const URL = 'https://tiktok-be.onrender.com'
 // const URL = 'http://localhost:3001'
 
-export const SUGGEST_ACOUNTS = `${URL}/api/suggest_accounts`
-export const FOLLOWING_ACOUNTS = `${URL}/api/following_accounts`
+export const SUGGEST_ACOUNTS = `${URL}/api/suggestAccounts`
+export const FOLLOWING_ACOUNTS = `${URL}/api/followingAccounts`
 export const DISCOVER = `${URL}/api/discover`
 export const NEWFEED = `${URL}/api/newfeed`
-export const USERS = `${URL}/api/detail_accounts`
+export const USERS = `${URL}/api/detailAccounts`
 export const FOLLOWING = `${URL}/api/following`
 export const HEARTED = `${URL}/api/hearted`
-export const POST_VIDEO = `${URL}/api/post_videos`
+export const POST_VIDEO = `${URL}/api/postVideos`
 export const ADD_USERS = `${URL}/api/add_users`
-export const DELETE_VIDEO = `${URL}/api/delete_videos`
+export const DELETE_VIDEO = `${URL}/api/deleteVideos`
